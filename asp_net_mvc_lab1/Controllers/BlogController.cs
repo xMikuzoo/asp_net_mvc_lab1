39d54c0d6b7473ab05e55a7d5afb3ab953c0f0d4
Wojciech Król 1 <wojciech.krol1@microsoft.wsei.edu.pl>
2025-10-05 19:02:31 +0200

    zad2

---
+++ asp_net_mvc_lab1/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using asp_net_mvc_lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_mvc_lab1.Controllers
+{
+    public class BlogController(ILogger<BlogController> logger) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new BlogViewModel() { 
+                Title = "WelcomePageExtensions to My Blod",
+                Description = "This is a asimple blog application build with ASP.NET MVC."
+            });
+        }
+    }
+}
---
+++ asp_net_mvc_lab1/Models/BlogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace asp_net_mvc_lab1.Models
+{
+    public class BlogViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
