466bb2188aad1c741845995825c66b9d4e575a54
wojciech.krol1 <wojciech.krol1@LA-09>
2025-10-05 18:30:42 +0200

    Dodaj pliki projektów.

---
+++ asp_net_mvc_lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp_net_mvc_lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
